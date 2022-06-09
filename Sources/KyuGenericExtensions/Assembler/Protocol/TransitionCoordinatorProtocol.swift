9595faaa0a396c10fd645aedbdbb7f6a6f0b393e
Chayanon Ardkham <develop@chayanon.dev>
2022-06-09 13:27:12 +0700

    Prototype - Any NavigationType on TransitionCoordinatorProtocol

--- Sources/KyuGenericExtensions/Assembler/Protocol/TransitionCoordinatorProtocol.swift
+++ Sources/KyuGenericExtensions/Assembler/Protocol/TransitionCoordinatorProtocol.swift
@@ -4,5 +4,23 @@
 //
 
 import Foundation
+import UIKit
 
-public typealias TransitionCoordinatorProtocol = ModuleProtocol & RouterProtocol
+public protocol TransitionCoordinatorProtocol: ModuleProtocol {
+	var resolver: ResolverProtocol { get }
+	func performNavigation(_ type: Any, animated: Bool, completion: (() -> Void)?)
+	func resolveScene(sceneName: String, parameters: [String: Any]?) -> UIViewController?
+}
+
+public extension TransitionCoordinatorProtocol {
+	func resolveScene(sceneName: String, parameters: [String: Any]?) -> UIViewController? {
+		guard let sceneModule = resolver.resolve(
+			SceneModuleProtocol.self,
+			name: sceneName
+		) else {
+			return nil
+		}
+		
+		return sceneModule.build(with: parameters)
+	}
+}
