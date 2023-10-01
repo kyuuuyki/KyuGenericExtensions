e1a3a29309e7e591f66ea8371c6f169de9e628b4
Chayanon Ardkham <develop@chayanon.dev>
2023-10-01 16:14:54 +0700

    Prototype - Update Extensions

---
+++ Sources/KyuGenericExtensions/PropertyWrapper/CodableIgnored.swift
@@ -0,0 +1,41 @@
+//
+//  CodableIgnored.swift
+//  KyuGenericExtensions
+//
+
+import Foundation
+
+@propertyWrapper
+public struct CodableIgnored<T>: Codable {
+	public var wrappedValue: T?
+	
+	public init(wrappedValue: T?) {
+		self.wrappedValue = wrappedValue
+	}
+	
+	public init(from decoder: Decoder) throws {
+		self.wrappedValue = nil
+	}
+	
+	public func encode(to encoder: Encoder) throws {
+		// Ignoring encoding process
+	}
+}
+
+public extension KeyedDecodingContainer {
+	func decode<T>(
+		_ type: CodableIgnored<T>.Type,
+		forKey key: Self.Key
+	) throws -> CodableIgnored<T> {
+		CodableIgnored(wrappedValue: nil)
+	}
+}
+
+public extension KeyedEncodingContainer {
+	mutating func encode<T>(
+		_ value: CodableIgnored<T>,
+		forKey key: KeyedEncodingContainer<K>.Key
+	) throws {
+		// Ignoring encoding process
+	}
+}
